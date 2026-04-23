bc28308544a9af3120bf23f6b13e7adc2febdc61
Jashanpreet singh <jashanpreetsinghj123+github.gmial.com>
2026-04-23 12:26:46 +0530

    data cleaning and feasture scaling start

--- model.py
+++ model.py
@@ -9,17 +9,48 @@ import datetime as dt
 import pickle
 import tensorflow as tf
 
-# print(tf.__version__)
+
 
 # data = yf.download('AAPL', start='2010-01-01', end=dt.datetime.now().strftime('%Y-%m-%d'))
 # data.reset_index().to_csv('apple.csv')
-# data.columns = data.columns.droplevel(1)
 
 
-ticker = yf.Ticker('AAPL')
-ticker.info
-# data = ticker.history('max')
+# data cleaning and preprocessing
+def data_cleaning():
+    df = pd.read_csv('apple.csv')
+    print(df.columns)
+    df = df.drop(columns=['Dividends', 'Stock Splits'])
+    df.to_csv('apple.csv', index=False)
+    print(df.columns)
+    print(df.head())
+    print(df.shape)
+    df['Date'] = pd.to_datetime(df['Date']).dt.tz_localize(None)
+    df = df.set_index('Date')
+    df.to_csv('apple.csv')
+    
+def feature_scaling():
+    df = pd.read_csv('apple.csv')
+    closing_price = df[['Close']].values  
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(closing_price)
+    with open('scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
+    return scaled_data  
+    
+    
+    
+    
+    
+# data_cleaning()
+feature_scaling()
+
+
+
+
+
+# print(ticker.info)
+
 # print(data)
 # print(data.head())
-# print(data.shape)
+# print(data.shape) 
 # print(data.info())
